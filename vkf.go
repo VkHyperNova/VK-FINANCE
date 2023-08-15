426c680a5e14d4a2769fbb9d3484de3e1940c691
Veikko Kiis <kiis.veikko@gmail.com>
2023-08-15 21:48:51 +0300

    Version 1 ready

--- vkf.go
+++ vkf.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
@@ -11,7 +12,17 @@ import (
 	"time"
 )
 
+type finance struct {
+	NET_WORTH float64 `json:"net_worth"`
+	BALANCE   float64 `json:"balance"`
+	EXPENSES  float64 `json:"expences"`
+	DATE      string `json:"date"`
+}
 
+var BALANCE float64 = 0
+var EXPENSES float64 = 0
+var NET_WORTH float64 = 0
+var DATE string 
 
 const (
 	Reset  = "\033[0m"
@@ -25,15 +36,15 @@ const (
 )
 
 func main() {
-
+	CHECK_DB()
 	CL()
 }
 
 func CL() {
 
 	PRINT_STATISTICS()
-
-	fmt.Println(Cyan + "\n<< COMMANDS: add | spent | grow | q >>" + Reset)
+	
+	fmt.Println(Cyan + "\n<< COMMANDS: add | exp | grow | q >>" + Reset)
 	fmt.Print("=> ")
 
 	reader := bufio.NewReader(os.Stdin)
@@ -45,8 +56,8 @@ func CL() {
 		switch command {
 		case "add":
 			Add()
-		case "spent":
-			Spent()
+		case "exp":
+			EXP()
 		case "grow":
 			Grow()
 		case "q":
@@ -58,79 +69,81 @@ func CL() {
 	}
 }
 
-
 /* Main Commands */
-var MONEY float64 = 0
-var SPENT float64 = 0
 
 func Add() {
-	MONEY = Question("Money: ")
+	BAL := Question("Money: ")
+	BALANCE = BALANCE + BAL
+	SAVE()
 	Clear_Screen()
 	CL()
 }
 
-func Spent() {
-	SPENT = Question("How much did you spend? ")
+func EXP() {
+	EXP := Question("How much did you spend? ")
+	EXPENSES = EXPENSES - EXP
+	SAVE()
 	Clear_Screen()
 	CL()
 }
 
+/* UNDER CONSTRUCTION */
 func Grow() {
-	
+	NET_WORTH = NET_WORTH + BALANCE
+	BALANCE = 0
+	SAVE()
 	Clear_Screen()
 	CL()
 }
 
 func PRINT_STATISTICS() {
 
+	SETUP()
+
 	fmt.Println()
 	fmt.Println(Cyan + "<<___________ VK FINANCE v1 ___________>>" + Reset)
+	fmt.Println(Cyan + "<<_____________________________________>>" + Reset)
+	fmt.Println()
+	fmt.Println(Cyan + "<< ", DATE, " >>" + Reset)
 	fmt.Println()
 
-	// DONE
+	fmt.Print(Cyan + "NET WORTH: " + Reset)
+	fmt.Println(Green, NET_WORTH, "EUR" + Reset)
+	fmt.Println()
+
+	fmt.Print(Cyan + "BALANCE: " + Reset)
+	fmt.Println(Yellow, BALANCE, "EUR" + Reset)
+
+	fmt.Print(Cyan + "EXPENSES: " + Reset)
+	fmt.Println(Red, EXPENSES, "EUR" + Reset)
+
 	CalculateDaylySpending()
+	MONEY_SAVER()
 }
-
-
-/* Constructors */
 
 func CalculateDaylySpending() {
-	DaysLeft := CalculateDaysLeft()
-	DayMaxSpending := TWO_DECIMAL_POINTS(MONEY/float64(DaysLeft))
-
-	// Convert to string
-	DaysLeftString := strconv.Itoa(DaysLeft)
-	
-	fmt.Println("Max Dayly Spending" + "(" + Yellow + DaysLeftString + Reset + ")" + " -> " + Yellow + DayMaxSpending + Reset + " EUR")
-}
-
-func CalculateDaysLeft() int {
 	now := time.Now()
 	currentYear, currentMonth, _ := now.Date()
 	currentLocation := now.Location()
 	firstDayOfMonth := time.Date(currentYear, currentMonth, 1, 0, 0, 0, 0, currentLocation)
 	lastDayOfMonth := firstDayOfMonth.AddDate(0, 1, -1)
 	DaysLeftBeforeEndOfMonth := lastDayOfMonth.Day() - now.Day()
-	DaysLeftBeforePayday := DaysLeftBeforeEndOfMonth + 6
+	DaysLeftBeforePayday := DaysLeftBeforeEndOfMonth + 5
 	DaysLeftBeforePayday = CheckWeekend(DaysLeftBeforePayday)
+	DayMaxSpending := TWO_DECIMAL_POINTS(BALANCE / float64(DaysLeftBeforePayday))
+	DaysLeftString := strconv.Itoa(DaysLeftBeforePayday)
 
-	return DaysLeftBeforePayday
+	fmt.Println()
+	fmt.Print(Cyan + "Max Dayly Spending (" + Reset)
+	fmt.Print(Yellow, DaysLeftString + Reset)
+	fmt.Print(Cyan + "): " + Reset)
+	fmt.Println(Yellow + DayMaxSpending + " EUR" + Reset)
 }
 
-func CheckWeekend(DaysLeftBeforePayday int) int {
-	var AddDays = DaysLeftBeforePayday
-	NextPayDayDate := time.Now().AddDate(0, 0, DaysLeftBeforePayday)
-	GetWeekDay := NextPayDayDate.Weekday()
-
-	if GetWeekDay == time.Saturday {
-		AddDays += 2
-	}
-
-	if GetWeekDay == time.Sunday {
-		AddDays += 1
-	}
-
-	return AddDays
+func MONEY_SAVER() {
+	Savings := BALANCE * 0.25
+	fmt.Print(Cyan + "SAVING (25%): " + Reset)
+	fmt.Println(Green + TWO_DECIMAL_POINTS(Savings) + " EUR" + Reset)
 }
 
 /* Other */
@@ -196,5 +209,102 @@ func Error(err error, location string) {
 		fmt.Println(" << Function name: ", location+" >>")
 		fmt.Println(err.Error())
 
+	}
+}
+
+func READ_FILE(filename string) []byte {
+	file, err := os.ReadFile(filename)
+	Error(err, "ReadFile")
+	return file
+}
+
+func WRITE_FILE(filename string, dataBytes []byte) {
+
+	var err = os.WriteFile(filename, dataBytes, 0644)
+	Error(err, "WRITE_FILE FUNCTION")
+}
+
+func DIR_CHECK(dir_name string) bool {
+
+	if _, err := os.Stat(dir_name); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func MAKE_DIR(dir_name string) {
+	_ = os.Mkdir(dir_name, 0700)
+}
+
+func CONVERT_TO_BYTE(data interface{}) []byte {
+	dataBytes, err := json.MarshalIndent(data, "", "  ")
+	Error(err, "Convert_To_Byte")
+
+	return dataBytes
+}
+
+func SAVE() {
+	data := CONCSTRUCT_FINANCE_JSON()
+	dataBytes := CONVERT_TO_BYTE(data)
+	WRITE_FILE("./finance.json", dataBytes)
+}
+
+func CONVERT_TO_FINANCE(body []byte) finance {
+
+	data := finance{}
+
+	err := json.Unmarshal(body, &data)
+	Error(err, "CONVERT_TO_FINANCE")
+
+	return data
+}
+
+func OPEN_DB() finance {
+	data := READ_FILE("./finance.json")
+	return CONVERT_TO_FINANCE(data)
+}
+
+func CHECK_DB() {
+	if !DIR_CHECK("./finance.json") {
+		NET_WORTH = Question("NET_WORTH? ")
+		BALANCE = Question("BALANCE? ")
+		SAVE()
+		CL()
+	}
+}
+
+func CheckWeekend(DaysLeftBeforePayday int) int {
+	var AddDays = DaysLeftBeforePayday
+	NextPayDayDate := time.Now().AddDate(0, 0, DaysLeftBeforePayday)
+	GetWeekDay := NextPayDayDate.Weekday()
+
+	if GetWeekDay == time.Saturday {
+		AddDays += 2
+	}
+
+	if GetWeekDay == time.Sunday {
+		AddDays += 1
+	}
+
+	return AddDays
+}
+
+func SETUP() {
+	DB := OPEN_DB()
+	NET_WORTH = DB.NET_WORTH
+	BALANCE = DB.BALANCE
+	EXPENSES = DB.EXPENSES
+	DATE = DB.DATE
+}
+
+func CONCSTRUCT_FINANCE_JSON() finance {
+	currentTime := time.Now()
+	timeString := currentTime.Format("2006-01-02 15:04:05")
+
+	return finance {
+		NET_WORTH: NET_WORTH,
+		BALANCE: BALANCE,
+		EXPENSES: EXPENSES,
+		DATE: timeString,
 	}
 }
